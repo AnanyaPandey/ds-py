5e066efa797b84a42f4367b9d3b3e48ef011d48a
Ananya <anany.boss@gmail.com>
2022-12-26 17:55:02 +0530

    stack using linkedlist

--- stack_linked_list.py
+++ stack_linked_list.py
@@ -19,16 +19,61 @@ class Node :
 class Linkedlist :
     def __init__(self):
         self.head = None
+        self.tail = None
+    
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
 
 class stacks:
     def __init__(self):
-        self.head = None
-        self.tail = None
+# when we are creating stack we are automatically creating a linkedlist
+        self.linkedlist = Linkedlist()
+
     
     def __str__(self):
-        node = self.head
-        while node is not None : 
-            node = node.next
-            p = ''.join(node.value)
-        return p
+        values = [str(x.value) for x in self.linkedlist]
+        return ' '.join(values)
     
+    def isempty(self):
+        if self.linkedlist.head == None : 
+            return True
+        else : 
+            return False
+    def push(self,value):
+        node = Node(value) # new node create
+        node.next = self.linkedlist.head # nodes next is current head
+        self.linkedlist.head = node # head's next points to node
+        # pushing new node as first point 
+        
+    def pop(self):
+        if self.isempty():
+            return 'list is empty'
+        else:
+            nodevalue = self.linkedlist.head.value
+            self.linkedlist.head = self.linkedlist.head.next
+            return nodevalue
+    
+    def top(self):
+        if self.isempty():
+            return 'list empty'
+        else:
+            nodevalue = self.linkedlist.head.value
+            return nodevalue
+    
+    def deletestack(self):
+        self.linkedlist.head = None
+        
+            
+        
+stack1 = stacks()
+stack1.push(22)
+stack1.push(33)
+stack1.push(55)
+
+print(stack1.isempty())
+print(stack1)
+stack1.pop()
+print(stack1)
