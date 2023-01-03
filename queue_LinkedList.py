2789e32f5a0a69274cc7db6ec65cd357e60cf81f
Ananya <anany.boss@gmail.com>
2023-01-03 19:25:23 +0530

    updating 030123

---
+++ queue_LinkedList.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 16 22:43:41 2021
+
+@author: ananyapa
+Queues : using linked lists
+When no one reference to a node X the node X gets garbage collected.
+"""
